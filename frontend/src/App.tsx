51744c9281ed0417cd400bd20af9a4d35fff9d80
Nick <niklas.braun30419@gmail.com>
2025-02-11 08:31:23 +0100

    Forgor to push App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -36,7 +36,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      <button onClick={() => getTestString(() => console.log('weather updated'))}>Test</button>
+      <button onClick={() => getTestString(() => console.log("weather updated"    ))}>Test</button>
       <p>{testString}</p>
     </>
   )
