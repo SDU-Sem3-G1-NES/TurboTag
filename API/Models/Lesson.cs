20af2595362d899ff5af9dec76d55d70ad71e664
Tokushiro <oskarandrzejewski16@gmail.com>
2025-02-26 18:03:29 +0100

    Created pocos for saving data as objects

---
+++ API/Models/Lesson.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Models
+{
+    public class Lesson
+    {
+        private int _id { get; set; }
+
+        private string tittle { get; set; }
+        private string? description { get; set; }
+        private string[]? tags { get; set; }
+        private string[]? category { get; set; }
+        private string? filetype { get; set; }
+    }
+}
