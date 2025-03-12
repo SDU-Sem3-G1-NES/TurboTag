4f345c6c906e63308e4768b43dcfbd487b23c419
benasbart <bbart23@student.sdu.dk>
2025-03-12 20:31:24 +0100

    oops

--- API/DependencyRegistrations.cs
+++ API/DependencyRegistrations.cs
@@ -12,21 +12,9 @@ public class DependencyRegistrations : Module
         builder.RegisterAssemblyTypes(typeof(Program).Assembly)
             .AsImplementedInterfaces()
             .Where(t => typeof(ITestBase).IsAssignableFrom(t));
-        // Automatically register all types that implement IAdminService
+        // Automatically register all types that implement IServiceBase
         builder.RegisterAssemblyTypes(typeof(Program).Assembly)
             .AsImplementedInterfaces()
-            .Where(t => typeof(IAdminService).IsAssignableFrom(t));
-        // Automatically register all types that implement ILibraryService
-        builder.RegisterAssemblyTypes(typeof(Program).Assembly)
-            .AsImplementedInterfaces()
-            .Where(t => typeof(ILibraryService).IsAssignableFrom(t));
-        // Automatically register all types that implement IUploadService
-        builder.RegisterAssemblyTypes(typeof(Program).Assembly)
-            .AsImplementedInterfaces()
-            .Where(t => typeof(IUploadService).IsAssignableFrom(t));
-        // Automatically register all types that implement IUserCredentialsService
-        builder.RegisterAssemblyTypes(typeof(Program).Assembly)
-            .AsImplementedInterfaces()
-            .Where(t => typeof(IUserCredentialsService).IsAssignableFrom(t));
+            .Where(t => typeof(IServiceBase).IsAssignableFrom(t));
     }
 }
