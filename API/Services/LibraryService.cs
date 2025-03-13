044da512a0480443986f435c40f1c08f2349d3b3
DovydasGan <dogan23@student.sdu.dk>
2025-03-13 14:18:46 +0100

    Fixed return object

--- API/Services/LibraryService.cs
+++ API/Services/LibraryService.cs
@@ -27,16 +27,16 @@ public class LibraryService : ILibraryService
     public List<UploadDTO> GetLibraryUploadsById()
     {
         return new List<UploadDTO>
-        {
-            new UploadDTO(1, 1, new UploadDetailsDTO(1, "Mock Description 1", "Mock Title 1", new List<string> { "Mock Tag 1" }), new FileMetadataDTO(1, "mp4", "Mock FileName 1", 2.5f, 1000, "2025-01-01", "Mock checkSum 1")),
-            new UploadDTO(2, 1, new UploadDetailsDTO(2, "Mock Description 2", "Mock Title 2", new List<string> { "Mock Tag 2" }), new FileMetadataDTO(2, "mp4", "Mock FileName 2", 2.5f, 1000, "2025-01-01", "Mock checkSum 2")),
-        };
+            {
+                new UploadDTO(1, 1, 1, new UploadDetailsDTO(1, "MockFile1.txt", "/mock/path/MockFile1.txt", new List<string>()), new FileMetadataDTO(1, "MockFile1.txt", "/mock/path/MockFile1.txt", 0.0f, null, "MockType", "MockDescription")),
+                new UploadDTO(2, 1, 1, new UploadDetailsDTO(2, "MockFile2.txt", "/mock/path/MockFile2.txt", new List<string>()), new FileMetadataDTO(2, "MockFile2.txt", "/mock/path/MockFile2.txt", 0.0f, null, "MockType", "MockDescription"))
+            };
     }
     /// <summary>
     /// Method that returns an UploadDTO object that belongs to a Library by Id.
     /// </summary>
     public UploadDTO GetLibraryUploadById()
     {
-        return new UploadDTO(1, 1, new UploadDetailsDTO(1, "Mock Description 1", "Mock Title 1", new List<string> { "Mock Tag 1" }), new FileMetadataDTO(1, "mp4", "Mock FileName 1", 2.5f, 1000, "2025-01-01", "Mock checkSum 1"));
+        return new UploadDTO(1, 1, 1, new UploadDetailsDTO(1, "MockFile.txt", "/mock/path/MockFile.txt", new List<string>()), new FileMetadataDTO(1, "MockFile.txt", "/mock/path/MockFile.txt", 0.0f, null, "MockType", "MockDescription"));
     }
 }
