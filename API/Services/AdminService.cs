be3a1a5fac37e67eb5e214e21f282036c87fd944
benasbart <bbart23@student.sdu.dk>
2025-03-13 17:33:01 +0100

    fixed indent

--- API/Services/AdminService.cs
+++ API/Services/AdminService.cs
@@ -2,18 +2,18 @@ using API.DTOs;
 
 namespace API.Services;
 public interface IAdminService : IServiceBase
-    {
-        List<UserDTO> GetAllUsers();
-        UserDTO GetUserById();
-        List<LibraryDTO> GetAllLibraries();
-        LibraryDTO GetLibraryById();
-        void CreateNewUser();
-        void CreateNewUsers();
-        void UpdateUserById();
-        void UpdateUsersByIds();
-        void DeleteUserById();
-        void DeleteUsersByIds();
-    }
+{
+    List<UserDTO> GetAllUsers();
+    UserDTO GetUserById();
+    List<LibraryDTO> GetAllLibraries();
+    LibraryDTO GetLibraryById();
+    void CreateNewUser();
+    void CreateNewUsers();
+    void UpdateUserById();
+    void UpdateUsersByIds();
+    void DeleteUserById();
+    void DeleteUsersByIds();
+}
 public class AdminService : IAdminService
 {
     /// <summary>
