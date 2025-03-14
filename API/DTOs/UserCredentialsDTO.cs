f2b16784c0fef208d78896fe0a510e996c08b029
Simas <simonas.tarasauskas@gmail.com>
2025-03-14 15:16:10 +0100

    Fix user credentials dto

--- API/DTOs/UserCredentialsDTO.cs
+++ API/DTOs/UserCredentialsDTO.cs
@@ -2,6 +2,6 @@ namespace API.DTOs;
 
 public class UserCredentialsDTO(string email, string password)
 {
-    public string Email { get; set; }
-    public string Password { get; set; }
+    public string Email { get; set; } = email;
+    public string Password { get; set; } = password;
 }
