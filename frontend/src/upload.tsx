66b85ebb81856ff2648e2eee3393176cbc9b3e8e
Tokushiro <oskarandrzejewski16@gmail.com>
2025-03-14 14:46:36 +0100

    Added Tag component

--- frontend/src/upload.tsx
+++ frontend/src/upload.tsx
@@ -1,30 +1,17 @@
 import React, { useState } from 'react'
+import Tags from './tags'
 
 const Upload: React.FC = () => {
   const [title, setTitle] = useState<string>('')
   const [description, setDescription] = useState<string>('')
   const [file, setFile] = useState<File | null>(null)
-  const [tags, setTags] = useState<string[]>([])
-  const [tagInput, setTagInput] = useState<string>('')
-
-  const handleAddTag = () => {
-    if (tagInput.trim() !== '') {
-      setTags([...tags, tagInput])
-      setTagInput('')
-    }
-  }
-
-  const handleRemoveTag = (index: number) => {
-    setTags(tags.filter((_, i) => i !== index))
-  }
-
+  
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault()
     const data = {
       title,
       description,
-      file,
-      tags
+      file
     }
     console.log(data)
   }
@@ -66,23 +53,7 @@ const Upload: React.FC = () => {
         required
       />
       <br />
-
-      <ul>
-        {tags.map((tag, index) => (
-          <li key={index}>
-            {tag}
-            <button type="button" onClick={() => handleRemoveTag(index)}>
-              Remove
-            </button>
-          </li>
-        ))}
-      </ul>
-      <br />
-
-      <input type="text" value={tagInput} onChange={(e) => setTagInput(e.target.value)} />
-      <button type="button" onClick={handleAddTag}>
-        Add Tag
-      </button>
+      <Tags/>
 
       <br />
       <button type="submit">Upload</button>
