ce6f1f1e872f47a405460cf165bd92f02a0390a9
Tokushiro <oskarandrzejewski16@gmail.com>
2025-03-14 14:16:03 +0100

    Moved thing correctly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,47 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
-import Upload from './upload'
-import Clicker from './Clicker'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useState } from 'react'
+import { TestClient } from './api/apiClient.ts'
 
 
 function App() {
-  
+  const [count, setCount] = useState(0)
+  const testClient = new TestClient()
+  const [testString, setTestString] = useState<string | null>(null)
+
+  const getTestString = (callback: () => void) => {
+    console.log('getWeather')
+    testClient.get().then((response) => {
+      console.log('response', response)
+      setTestString(response)
+      callback()
+    })
+  }
 
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/upload">Upload</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Clicker/>}
-        />
-        <Route path="/upload" element={<Upload />} />
-      </Routes>
-    </Router>
+    <div>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <button onClick={() => getTestString(() => console.log('weather updated'))}>
+        Test
+      </button>
+      <p>{testString}</p>
+    </div>
   )
 }
 
