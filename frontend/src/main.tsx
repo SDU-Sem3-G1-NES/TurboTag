8a5665295aa18e33808d8940ae03842243aa6e49
Tokushiro <144448164+Tokushiro@users.noreply.github.com>
2025-03-17 17:03:44 +0100

    Create-Upload-Page (#10)
    * Created Upload Skeleton
    
    ---------
    
    Co-authored-by: GitHub Actions <actions@github.com>
    Co-authored-by: Nick <niklas.braun30419@gmail.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App.tsx'
+import Upload from './upload'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/upload">Upload</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/upload" element={<Upload />} />
+      </Routes>
+    </Router>
   </StrictMode>
 )
