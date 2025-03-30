24bc893d7927b82651c0ed229c5e17db797070ea
benasbart <bbart23@student.sdu.dk>
2025-03-30 20:39:40 +0200

    added simple page to retrieve all libraries for user, and see what uploads there are in it

---
+++ frontend/src/library.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+import { LibraryDTO, UploadDTO } from './api/apiClient'
+import { ContentLibraryClient } from './api/apiClient'
+
+const Library: React.FC = () => {
+    const contentLibraryClient = new ContentLibraryClient()
+    const [userLibraries, setUserLibraries] = useState<LibraryDTO[]>([])
+    const [libaryUploads, setLibraryUploads] = useState<UploadDTO[]>([])
+    const [selectedLibraryId, setSelectedLibraryId] = useState<string>('')
+    const fetchUserLibraries = () => {
+        const response = contentLibraryClient.getUserLibrariesById("mockId")
+        response.then((response) => {
+            setUserLibraries(response)  
+        })
+    }
+    const fetchLibraryUploads = (libraryId: string) => {
+        const response = contentLibraryClient.getLibraryUploadsById(libraryId)
+        response.then((response) => {
+            setLibraryUploads(response)  
+        })
+    }
+    useEffect(() => {
+      fetchUserLibraries()
+  }, []) 
+  return (
+    <div>
+    <p>Select a library to retrieve uploads from:</p>
+    <select value={selectedLibraryId} onChange={(e) => setSelectedLibraryId(e.target.value)}>
+        {userLibraries.map((library) => (
+            <option key={library.id} value={library.id}>
+                {library.name}
+            </option>
+        ))}
+    </select>
+    <div>
+        <button onClick={() => fetchLibraryUploads(selectedLibraryId)}>Fetch Uploads</button>
+        {libaryUploads.map((upload) => (
+            <div >
+              <h3>Upload ID: {upload.id}</h3>
+              <p>Owner ID: {upload.ownerId}</p>
+              <p>Library ID: {upload.libraryId}</p>
+            </div>
+        ))}
+    </div>
+    </div>
+  )
+}
+
+export default Library
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App.tsx'
 import Upload from './upload'
+import Library from './library'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
@@ -17,11 +18,15 @@ createRoot(document.getElementById('root')!).render(
           <li>
             <Link to="/upload">Upload</Link>
           </li>
+          <li>
+            <Link to="/library">Library</Link>
+          </li>
         </ul>
       </nav>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/upload" element={<Upload />} />
+        <Route path="/library" element={<Library />} />
       </Routes>
     </Router>
   </StrictMode>
