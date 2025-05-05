e80063dc62ae159ff04105c5c1fe349bd9638b6d
Simas <simonas.tarasauskas@gmail.com>
2025-05-05 15:59:35 +0200

    Audio track splitting into chunks.

--- API/Services/FFmpegService.cs
+++ API/Services/FFmpegService.cs
@@ -5,8 +5,7 @@ namespace API.Services;
 public interface IFFmpegService : IServiceBase
 {
     public Task<String> SaveFileToTemp(IFormFile file, string mongoId);
-    public Task<(String, List<String>)> GetVideoAudioAndSnapshots(string videoPath, string outputId, bool deleteInputFile);
-
+    public Task<(List<String>, List<String>)> GetVideoAudioAndSnapshots(string videoPath, string outputId, bool deleteInputFile);
 }
 public class FFmpegService : IFFmpegService
 {
@@ -17,7 +16,7 @@ public class FFmpegService : IFFmpegService
         Directory.CreateDirectory(_tempFolderPath); // Ensure the folder exists
     }
 
-    public async Task<(String, List<String>)> GetVideoAudioAndSnapshots(string videoPath, string outputId, bool deleteInputFile)
+    public async Task<(List<String>, List<String>)> GetVideoAudioAndSnapshots(string videoPath, string outputId, bool deleteInputFile)
     {
         var outputFolderPath = Path.Combine(_tempFolderPath, outputId);
         Directory.CreateDirectory(outputFolderPath);
@@ -27,33 +26,77 @@ public class FFmpegService : IFFmpegService
 
         FFMpeg.ExtractAudio(videoPath, audioPath);
 
+        var audioPaths = await SplitAudioTrack(audioPath);
+        
         var mediaInfo = await FFProbe.AnalyseAsync(videoPath);
         int snapshotPeriod = (int)mediaInfo.Duration.TotalSeconds/5;
 
-        for (int i = 1; i <= 5; i++)
+        for (int i = 0; i < 5; i++)
         {
-            await FFMpeg.SnapshotAsync(videoPath, Path.Combine(outputFolderPath, $"Snapshot-{i}.jpg"), null, TimeSpan.FromSeconds(i*snapshotPeriod));
+            var snapshotPath = Path.Combine(outputFolderPath, $"Snapshot-{i+1}.jpg");
+            await FFMpeg.SnapshotAsync(videoPath, snapshotPath, null, TimeSpan.FromSeconds(i*snapshotPeriod));
+            snapshotPaths.Add(snapshotPath);
         }
-
+        
         if (deleteInputFile)
         {
             File.Delete(videoPath);
         }
-
-        return (audioPath, snapshotPaths);
+        
+        return (audioPaths, snapshotPaths);
     }
-
+    
     public async Task<String> SaveFileToTemp(IFormFile file, string mongoId)
     {
         var outputFolderPath = Path.Combine(_tempFolderPath, mongoId);
         Directory.CreateDirectory(outputFolderPath);
 
         var tempFilePath = Path.Combine(_tempFolderPath, $"{mongoId}.tmp");
+        
         await using (var stream = new FileStream(tempFilePath, FileMode.Create))
         {
             await file.CopyToAsync(stream);
         }
+        
         return tempFilePath;
+    }
+    
+    private async Task<List<String>> SplitAudioTrack(string audioPath)
+    {
+        var outputPaths = new List<string>();
+        
+        var mediaInfo = await FFProbe.AnalyseAsync(audioPath);
+        var bitrate = mediaInfo.AudioStreams.First().BitRate;
+        int chunkDuration = 192000000 / (int)bitrate; // Duration in seconds for 24MB
+        
+        if (mediaInfo.Duration.TotalSeconds > chunkDuration)
+        {
+            var outputFolderPath = Path.GetDirectoryName(audioPath);
+            int chunkIndex = 0;
+            TimeSpan startTime = TimeSpan.Zero;
+        
+            while (startTime < TimeSpan.FromSeconds(mediaInfo.Duration.TotalSeconds))
+            {
+                var outputChunkPath = Path.Combine(outputFolderPath!, $"Audio-{chunkIndex + 1}.mp3");
+                outputPaths.Add(outputChunkPath);
+            
+                TimeSpan endTime = startTime.Add(TimeSpan.FromSeconds(chunkDuration));
+            
+                await FFMpeg.SubVideoAsync(audioPath, outputChunkPath, startTime, endTime);
+                
+                startTime = endTime;
+                chunkIndex++;
+            }
+            
+            File.Delete(audioPath);
+        }
+        
+        else
+        {
+            outputPaths.Add(audioPath);
+        }
+        
+        return outputPaths;
     }
     
 }
