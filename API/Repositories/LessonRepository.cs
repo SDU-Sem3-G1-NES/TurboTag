31d7fe1ac28733e0c9b2271b7a5d18910e9c7888
Simas <144455107+s1masy@users.noreply.github.com>
2025-05-21 19:25:09 +0200

    Filter-by-Uploader-fix (#59)
    * fix filter by upload in library page
    
    * add indent

--- API/Repositories/LessonRepository.cs
+++ API/Repositories/LessonRepository.cs
@@ -276,7 +276,7 @@ public class LessonRepository(IMongoDataAccess database) : ILessonRepository
         var results = database.Aggregate("lesson", pipeline);
 
         var dict = results
-            .Where(doc => doc.Contains("_id") && doc.Contains("owner_name"))
+            .Where(doc => doc.Contains("_id") && doc.Contains("owner_name") && doc["_id"].IsInt32 && doc["owner_name"].IsString)
             .ToDictionary(
                 doc => doc["_id"].AsInt32,
                 doc => doc["owner_name"].AsString
